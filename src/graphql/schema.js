52bd6b50c9b1c51663f1a3ea200088faf494153f
inPhoenix <spiderk@gmail.com>
2023-04-02 15:37:29 -0300

    :railway_car: using dataLoader to cache rests

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -3,7 +3,7 @@ import { userTypeDefs } from "./user/typdefs"
 import { userResolvers } from "./user/resolvers"
 import { postTypeDefs } from "./post/typdefs"
 import { postResolvers } from "./post/resolvers"
-import {apiFiltersTypeDefs} from "./api-filters/typedefs";
+import { apiFiltersTypeDefs } from "./api-filters/typedefs"
 
 const rootTypeDefs = gql`
   type Query {
@@ -19,5 +19,10 @@ const rootResolvers = {
   },
 }
 
-export const typedef = [rootTypeDefs, userTypeDefs, postTypeDefs, apiFiltersTypeDefs]
+export const typedef = [
+  rootTypeDefs,
+  userTypeDefs,
+  postTypeDefs,
+  apiFiltersTypeDefs,
+]
 export const resolvers = [rootResolvers, userResolvers, postResolvers]
